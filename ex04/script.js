608078d5fe164831284d2490fcee6232a5172f54
Beatriz Martins <beatrizmartins3252@gmail.com>
2024-09-10 22:57:15 +0000

    codigo atualizado

--- ex04/script.js
+++ ex04/script.js
@@ -1,4 +1,4 @@
-let btnAdicionar = document.getElementById('adicionar');
+const btnAdicionar = document.getElementById('adicionar');
 arrayDeNumeros = [];
 let somatorio = 0;
 
@@ -6,16 +6,13 @@ btnAdicionar.onclick = function (){
     let numero = document.getElementById('input-numero').value;
     arrayDeNumeros.push(numero);
 
-    const jsonArray = JSON.stringify(arrayDeNumeros);
+    let jsonArray = JSON.stringify(arrayDeNumeros);
     document.getElementById('array').textContent = jsonArray;
 
-    for (const i = 0; i <= arrayDeNumeros.length; i++)
-            somatorio += Number(numero);
-            let media = (somatorio / arrayDeNumeros.length)
-            console.log(media);
-    
-    }
-    
- 
-    
+    somatorio += Number(numero)
+
+    let media = (somatorio / arrayDeNumeros.length)
+
+    document.getElementById('media').textContent = media;
+}
 
